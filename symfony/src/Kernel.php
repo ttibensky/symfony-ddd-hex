14d3f3b5ab69badc1b0c95e3438ccd7453413e5d
Tomas Tibensky <ttibensky@gmail.com>
2023-10-07 16:01:07 +0200

    Add cache and logs dir configuration

--- symfony/src/Kernel.php
+++ symfony/src/Kernel.php
@@ -8,4 +8,14 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public function getCacheDir(): string
+    {
+        return sys_get_temp_dir() . '/symfony/var/' . $this->environment . '/cache';
+    }
+
+    public function getLogDir(): string
+    {
+        return sys_get_temp_dir() . '/symfony/var/' . $this->environment . '/log';
+    }
 }
